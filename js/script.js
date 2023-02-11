712885f10ab0cfd3a3e03125cfe42c2b2c8c4992
Mateusz Grincewicz <matek160393@gmail.com>
2023-02-11 09:08:30 +0100

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
 
     for (const task of tasks) {
       htmlString += `
-        <li class="section__task ${task.done ? "section__task--done" : "" }">
+        <li class="section__task ${task.done ? "section__task--done" : ""}">
           ${task.content}
         </li>
         `;
@@ -22,8 +22,34 @@
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   }
+
+  const addNewTask = (newTaskContent) => {
+
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-form__newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+
+  };
+
   const init = () => {
     render();
-  }
+
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+  };
   init();
 }
